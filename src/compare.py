084fa193cff92eb28798a5b210e13512aa6a14d5
MistaRae <itsamistarae@gmail.com>
2021-04-07 23:41:00 +0100

    third test & function written and working

--- src/compare.py
+++ src/compare.py
@@ -1,5 +1,7 @@
 def compare(number_1, number_2):
+    if number_1 == number_2:
+        return "both numbers are equal"
     if number_1 < number_2:
         return f"{number_1} is less than {number_2}"
     return f"{number_1} is greater than {number_2}"
-    pass
+    
