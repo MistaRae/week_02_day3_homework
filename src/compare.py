7102b018f979b6b2fbe0666a18b2a09342874b77
MistaRae <itsamistarae@gmail.com>
2021-04-07 23:43:21 +0100

    refactored function to me more concise

--- src/compare.py
+++ src/compare.py
@@ -1,7 +1,12 @@
 def compare(number_1, number_2):
-    if number_1 == number_2:
-        return "both numbers are equal"
+    # if number_1 == number_2:
+    #     return "both numbers are equal"
+    # if number_1 < number_2:
+    #     return f"{number_1} is less than {number_2}"
+    # return f"{number_1} is greater than {number_2}"
+    
     if number_1 < number_2:
         return f"{number_1} is less than {number_2}"
-    return f"{number_1} is greater than {number_2}"
-    
+    elif number_1 > number_2:
+        return f"{number_1} is greater than {number_2}"
+    return "both numbers are equal"
