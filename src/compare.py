d7d55baa877df8bd7797ec1257e18254b581a598
MistaRae <itsamistarae@gmail.com>
2021-04-07 23:36:53 +0100

    first and seconds tests passed

--- src/compare.py
+++ src/compare.py
@@ -1,2 +1,5 @@
-def compare():
+def compare(number_1, number_2):
+    if number_1 < number_2:
+        return f"{number_1} is less than {number_2}"
+    return f"{number_1} is greater than {number_2}"
     pass
